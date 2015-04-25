96b683efaf1daa6f54149a581795400caa31c14c
Carl Jeske <carl.jeske@cern.ch>
2015-04-25 13:23:16 +0100

    Add weather data.  - loadData() now loads weather data from Chicago O'Hare and merges it into the input data  - Only retains temperature and precipitation information  - Precip. data contains NAs

--- west-nile-virus-prediction/loadData.R
+++ west-nile-virus-prediction/loadData.R
@@ -28,9 +28,6 @@ loadData <- function() {
   # Ignore satellite traps, whose trap names are ended with a letter
   input <- subset(input, nchar(Trap) == 4)
   
-  # Save WNV presence as a bool
-  #input$WnvPresent <- as.logical(input$WnvPresent)
-  
   # Perform mean normalisation
   input <- meanNormalise(input, 4)
   input <- meanNormalise(input, 5)
@@ -38,6 +35,27 @@ loadData <- function() {
   # Save day of year (0-366)
   input$DayOfYear <- as.integer(format(input$Date, "%j"))
   
+  # Load weather data
+  weatherData <- loadWeatherData()
+  
+  # Merge in weather data from station 1 (Chicago O'Hare)
+  input <- merge(data,
+                 weatherData[weatherData$Station == 1, ],
+                 all.x = TRUE)
+  
+  input <- na.omit(input)
+  
   return(input)
 }
 
+loadWeatherData <- function() {
+  weatherData <- read.csv("data/weather.csv",
+                          stringsAsFactors = FALSE,
+                          na.strings = c("M", "  T"))
+  weatherData <- transform(weatherData,
+                           Station = as.factor(Station),
+                           Date = as.Date(Date))
+  
+  # Reorder and save temp. and precip. data
+  weatherData <- weatherData[, c(2, 1, 4, 3, 5, 17)]
+}
