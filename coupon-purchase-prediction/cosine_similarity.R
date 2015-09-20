02419026e843756867861d9708e957f32d338b22
Carl Jeske <carl.jeske@cern.ch>
2015-09-20 15:27:00 +0100

    Extensive re-write of cosine similarity code
    * Weights read in from file
    * Scores converted using sigmoid function
    * Refactor some subroutines into functions
    * Add memory hungry code to calculate y and J
    * Add debug mode which uses 100 users and test coupons
    * Add bias node with 0 weight
    * Add dummy ideal coupon values for users with no purchase history

--- coupon-purchase-prediction/cosine_similarity.R
+++ coupon-purchase-prediction/cosine_similarity.R
@@ -1,11 +1,63 @@
 ### Kaggle Scripts: Ponpare Coupon Purchase Prediction ###
 ### Author: Subhajit Mandal ###
 
+require(Matrix)
+
+sigmoid <- function(x) {
+    return (1 / (1 + exp(-x)))
+}
+
+getWeightMatrix <- function(x) {
+    #Weight Matrix: GENRE_NAME DISCOUNT_PRICE PRICE_RATE USABLE_DATE_ large_area_name ken_name small_area_name BIAS
+    return (as.matrix(Diagonal(x = x)))
+}
+
+getHyp <- function(weights, user_prefs, coupons) {
+    weight_matrix <- getWeightMatrix(weights)
+    hyp <- as.matrix(user_prefs[,2:ncol(user_prefs)]) %*% weight_matrix %*% t(as.matrix(coupons[,2:ncol(coupons)]))
+    hyp <- sigmoid(hyp)
+    
+    return (hyp)
+}
+
+getCost <- function(y, weights, user_prefs, coupons) {
+    hyp <- getHyp(weights, user_prefs, coupons)
+    
+    #return (sum(0.5 * (hyp - y)**2) / length(y))
+    
+    return ((-1 / length(y)) * (sum(y * log(hyp) + (1 - y) * log(1 - hyp))))
+}
+
+saveSubmission <- function(weights, user_prefs, coupons, full_user_list) {
+    hyp <- getHyp(weights, user_prefs, coupons)
+    
+    #order the list of coupons according to similairties and take only first 10 coupons
+    user_prefs$PURCHASED_COUPONS <- do.call(rbind, lapply(1:nrow(user_prefs),FUN=function(i){
+        purchased_cp <- paste(coupons$COUPON_ID_hash[order(hyp[i,], decreasing = TRUE)][1:10],collapse=" ")
+        return(purchased_cp)
+    }))
+
+    #make submission
+    submission <- merge(full_user_list, user_prefs, all.x=TRUE)
+    submission <- submission[,c("USER_ID_hash","PURCHASED_COUPONS")]
+    write.csv(submission, file="cosine_sim.csv", row.names=FALSE)
+}
+
+debug <- FALSE
+weight_vector <- scan('weight_vector.txt')
+
 #read in all the input data
 coupon_detail_train <- read.csv("input/coupon_detail_train.csv")
 coupon_list_train <- read.csv("input/coupon_list_train.csv")
 coupon_list_test <- read.csv("input/coupon_list_test.csv")
 user_list <- read.csv("input/user_list.csv")
+
+if (debug) {
+    user_list <- user_list[1:100, ]
+    coupon_detail_train <- coupon_detail_train[coupon_detail_train$USER_ID_hash %in% user_list$USER_ID_hash, ]
+    coupon_list_train <- coupon_list_train[coupon_list_train$COUPON_ID_hash %in% coupon_detail_train$COUPON_ID_hash, ]
+    coupon_list_test <- coupon_list_test[1:100, ]
+}
 
 #making of the training set
 training_coupons <- merge(coupon_detail_train,coupon_list_train)
@@ -32,29 +84,30 @@ training_coupons$PRICE_RATE <- (training_coupons$PRICE_RATE*training_coupons$PRI
 training_coupons <- cbind(training_coupons[,c(1,2)],model.matrix(~ -1 + .,training_coupons[,-c(1,2)],
                                                     contrasts.arg=lapply(training_coupons[,names(which(sapply(training_coupons[,-c(1,2)], is.factor)==TRUE))], contrasts, contrasts=FALSE)))
 
+#add bias node
+training_coupons$BIAS <- 1
+
 #separate the test from training_coupons
 testing_coupons <- training_coupons[training_coupons$USER_ID_hash=="dummyuser",]
 testing_coupons <- testing_coupons[,-2]
 training_coupons <- training_coupons[training_coupons$USER_ID_hash!="dummyuser",]
 
+#y <- matrix(nrow = nrow(user_list), ncol = nrow(coupon_list_train))
+#for (i in 1:nrow(coupon_list_train)) {
+#    coupon <- as.character(sort(coupon_list_train$COUPON_ID_hash)[i])
+#    y[, i] <- as.numeric(sort(user_list$USER_ID_hash) %in% training_coupons[training_coupons$COUPON_ID_hash == coupon, 'USER_ID_hash'])
+#}
+
 #data frame of user characteristics
 user_ideal_coupon <- aggregate(.~USER_ID_hash, data=training_coupons[,-1],FUN=mean)
+user_ideal_coupon <- merge(data.frame(USER_ID_hash = sort(user_list$USER_ID_hash)), user_ideal_coupon, all.x = TRUE)
+user_ideal_coupon[is.na(user_ideal_coupon)] <- 0
 user_ideal_coupon$DISCOUNT_PRICE <- 1
 user_ideal_coupon$PRICE_RATE <- 1
+#training_coupons <- aggregate(.~COUPON_ID_hash, data=training_coupons[,-2],FUN=mean)
 
-#Weight Matrix: GENRE_NAME DISCOUNT_PRICE PRICE_RATE USABLE_DATE_ large_area_name ken_name small_area_name
-require(Matrix)
-W <- as.matrix(Diagonal(x=c(rep(2.05,13), rep(2.01,1), rep(-0.13,1), rep(0,9), rep(0.51,9), rep(1.01,47), rep(4.85,55))))
+#calculation of cosine similarities of users and coupons
+#test_scores <- getHyp(weight_vector, user_ideal_coupon, testing_coupons)
+#train_scores <- getHyp(weight_vector, user_ideal_coupon, training_coupons)
 
-#calculation of cosine similairties of users and coupons
-score = as.matrix(user_ideal_coupon[,2:ncol(user_ideal_coupon)]) %*% W %*% t(as.matrix(testing_coupons[,2:ncol(testing_coupons)]))
-#order the list of coupons according to similairties and take only first 10 coupons
-user_ideal_coupon$PURCHASED_COUPONS <- do.call(rbind, lapply(1:nrow(user_ideal_coupon),FUN=function(i){
-  purchased_cp <- paste(testing_coupons$COUPON_ID_hash[order(score[i,], decreasing = TRUE)][1:10],collapse=" ")
-  return(purchased_cp)
-}))
-
-#make submission
-submission <- merge(user_list, user_ideal_coupon, all.x=TRUE)
-submission <- submission[,c("USER_ID_hash","PURCHASED_COUPONS")]
-write.csv(submission, file="cosine_sim.csv", row.names=FALSE)
+saveSubmission(weight_vector, user_ideal_coupon, testing_coupons, user_list)
